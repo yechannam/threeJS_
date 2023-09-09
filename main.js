3c84fe6a449b13610e15d97ea722dba236483275
Nam Yechan <chanmias0318@gmail.com>
2023-09-09 15:17:37 +0900

    ADD : rotate botton

--- main.js
+++ main.js
@@ -1,95 +1,41 @@
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import * as THREE from 'three';
 
-// const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+let rotationIt = true;
 
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize( window.innerWidth, window.innerHeight );
-// document.body.appendChild( renderer.domElement );
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-// camera.position.set(5, 14, 18);
-// camera.lookAt( 0.5, 10, 16 );
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-// light = new THREE.AmbientLight(0xffffff); // soft white light
-// scene.add(light);
+camera.position.set(0,3,10);
 
-// light_sun = new THREE.DirectionalLight ( 0x808080, 1.0 );
-// var shadowBlur=10;
-// light_sun.castShadow=true;
-// light_sun.shadow.camera.left=-shadowBlur;
-// light_sun.shadow.camera.right=shadowBlur;
-// light_sun.shadow.camera.top=shadowBlur;
-// light_sun.shadow.camera.bottom=-shadowBlur;
-// scene.add( light_sun );
+let loader = new GLTFLoader();
 
-// let loader = new GLTFLoader();
-// loader.load('why.gltf', function(gltf){
-//     Mesh = gltf.scene;
-//             Mesh.scale.set(1,1,1);
-//             scene.add(Mesh);
-//             Mesh.position.x = 0;
-//             Mesh.position.y = 10;
-//             Mesh.position.z = 15;
-//     renderer.render(scene, camera);
-// });
-
-const renderer = new THREE.WebGLRenderer( { canvas: MyCanvas } );
-    const camera = new THREE.PerspectiveCamera(50, 640 / 180, 1, 1000);
-    const scene = new THREE.Scene();
-    let Mesh;
-    let light;
-    let light_sun;
-
-    function init() {
-        scene.background = new THREE.Color('black');
-        camera.position.set(5, 14, 18);
-        camera.lookAt( 0.5, 10, 16 );
-        // renderer.setSize(window.innerWidth, window.innerHeight);
-        // document.body.appendChild(renderer.domElement);
-    }
-
-    function setLight() {
-        light = new THREE.AmbientLight(0xffffff); // soft white light
-        scene.add(light);
-        
-        light_sun = new THREE.DirectionalLight ( 0x808080, 1.0 );
-          var shadowBlur=10;
-          light_sun.castShadow=true;
-          light_sun.shadow.camera.left=-shadowBlur;
-          light_sun.shadow.camera.right=shadowBlur;
-          light_sun.shadow.camera.top=shadowBlur;
-          light_sun.shadow.camera.bottom=-shadowBlur;
-          scene.add( light_sun );
-    }
-
-    function loadGLTF() {
-        let balloonLoader = new THREE.GLTFLoader();
-
-        balloonLoader.load('why.gltf', (gltf) => {
-            Mesh = gltf.scene;
-            Mesh.scale.set(1,1,1);
-            scene.add(Mesh);
-            Mesh.position.x = 0;
-            Mesh.position.y = 10;
-            Mesh.position.z = 15;
-        });
-    }
-
+loader.load('cute_ghost/scene.gltf', function(gltf){
+    const obj = gltf.scene;
+    scene.add(obj);
+    
     function animate() {
-        requestAnimationFrame(animate);
-        if (Mesh && Mesh.rotation) {
-            Mesh.rotation.y -= 0.001;
-        }
-        
-        // 카메라 컨트롤러 추가
-        //let controls = new THREE.OrbitControls (camera, renderer.domElement);
-        //controls.update();
-      
-        renderer.render(scene, camera);
-    }
+        requestAnimationFrame( animate );
 
-    init();
-    setLight();
-    loadGLTF();
+        if (rotationIt)
+            obj.rotation.y += 0.01;
+        else
+            obj.rotation.y = 0;
+
+        renderer.render( scene, camera );
+    }
     animate();
+});
+
+const button = document.querySelector('button');
+ 
+const buttonClickHandler = () =>{
+  rotationIt = !rotationIt;
+};
+ 
+button.onclick = buttonClickHandler;
+
